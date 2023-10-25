939ba5fb5d2ee3055d0bf194a66cd3ed3209e7ed
kyoohyun <kgh2120@naver.com>
2023-10-25 19:49:06 +0900

    init project

---
+++ src/main/java/com/example/springtrash/SpringTrashApplication.java
@@ -0,0 +1,11 @@
+package com.example.springtrash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrashApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTrashApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/springtrash/SpringTrashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtrash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
